f48ad0b73d3548303320a9c9d0e5f13e18c8f184
Mikhail.Brinchuk <Mikhail.Brinchuk@localhost>
2012-02-08 17:25:35 +0400

    Push constraints

--- Generator.Core/IntegralPositiveMultiplicationGenerator.cs
+++ Generator.Core/IntegralPositiveMultiplicationGenerator.cs
@@ -9,24 +9,21 @@ namespace Generator.Core
 			double leftComplexity = context.ProbabilityGenerator.GetProbability() * context.MaxComplexity;
 			double rightComplexity = context.MaxComplexity - leftComplexity;
 
-			context.PushConstraint( new NotVerbatimConstantConstraint<int>( 0 ) );
-			context.PushConstraint( new NotVerbatimConstantConstraint<int>( 1 ) );
+			using ( context.PushConstraints( new NotVerbatimConstantConstraint<int>( 0 ), new NotVerbatimConstantConstraint<int>( 1 ) ) )
+			{
+				var left = context.ParentGenerator.Generate(
+					context
+						.CloneWithMaxComplexity(leftComplexity)
+						.CloneWithRange(Range.Create(1, 9)));
 
-			var left = context.ParentGenerator.Generate(
-				context
-				.CloneWithMaxComplexity( leftComplexity )
-				.CloneWithRange( Range.Create( 1, 9 ) ) );
+				var right = context.ParentGenerator.Generate(
+					context
+						.CloneWithMaxComplexity(rightComplexity)
+						.CloneWithRange(Range.Create(1, 9)));
 
-			var right = context.ParentGenerator.Generate(
-				context
-					.CloneWithMaxComplexity( rightComplexity )
-					.CloneWithRange( Range.Create( 1, 9 ) ) );
-
-			context.PopConstraint();
-			context.PopConstraint();
-
-			var result = new MultiplyOperation<int>( left, right );
-			return result;
+				var result = new MultiplyOperation<int>( left, right );
+				return result;
+			}
 		}
 
 		public override double Complexity
